11eb6cdd837e17ce8f4aaf76921e85b07e09ea86
GeorgeBoboc254 <ghitzaboboc@gmail.com>
2023-11-21 18:51:05 +0200

    Added NPCs for map I, implemented hitboxes and sounds

--- Assets/Scripts/AnimationStateController.cs
+++ Assets/Scripts/AnimationStateController.cs
@@ -10,19 +10,20 @@ public class AnimationStateController : MonoBehaviour
     public float rotationSpeed = 5f;
     public float walkSpeed = 10.0f;
     private float seenRange = 100f;
-    private float attackRange = 50f;
+    private float attackRange = 25f;
+    public AudioSource deathSound;
 
     void Start()
     {
-        target = Camera.main.transform;
+        //target = Camera.main.transform;
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        FollowTarget();
-       
+        if(animator.GetBool("isDead") == false)
+            FollowTarget();
     }
 
     private void FollowTarget()
@@ -42,7 +43,6 @@ public class AnimationStateController : MonoBehaviour
             animator.SetBool("isAttacking", true);
             return;
         }
-
         Vector3 direction = target.position - transform.position;
         direction.Normalize();
         direction.y = 0;
@@ -52,5 +52,22 @@ public class AnimationStateController : MonoBehaviour
         var lookRotation = Quaternion.LookRotation(direction);
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
         transform.Translate(direction * walkSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Axe"))
+        {
+            animator.SetBool("isDead", true);
+            StartCoroutine(PlayDeathSoundWithDelay(0.7f));
+        }
+    }
+    private IEnumerator PlayDeathSoundWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); // Wait for the specified delay
+        if (deathSound != null)
+        {
+            deathSound.Play(); // Play the death sound effect after the delay
+        }
     }
 }
